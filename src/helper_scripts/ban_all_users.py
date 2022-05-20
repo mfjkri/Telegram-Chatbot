82a6bc489af9cbfda5f3c73b3f5746c76a5feed0
mfjkri <muhammadfikri6844@gmail.com>
2022-05-20 11:26:46 +0800

    Moved script logic into main function

--- src/helper_scripts/ban_all_users.py
+++ src/helper_scripts/ban_all_users.py
@@ -9,10 +9,11 @@ users_directory = os.path.join("users")
 banned_users_file = os.path.join(users_directory, "banned_users.yaml")
 banned_users = load_yaml_file(banned_users_file) or []
 
-for chatid in os.listdir(users_directory):
-    user_directory = os.path.join(users_directory, chatid)
+if __name__ == "__main__":
+    for chatid in os.listdir(users_directory):
+        user_directory = os.path.join(users_directory, chatid)
 
-    if os.path.isdir(user_directory) and chatid not in banned_users:
-        banned_users.append(chatid)
+        if os.path.isdir(user_directory) and chatid not in banned_users:
+            banned_users.append(chatid)
 
-dump_to_yaml_file(banned_users, banned_users_file)
+    dump_to_yaml_file(banned_users, banned_users_file)
