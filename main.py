df6739b9d5c7a9e67cebc9271ebf14bddd51a750
mfjkri <muhammadfikri6844@gmail.com>
2022-05-26 18:05:48 +0800

    Added reply_message argument to end_of_chatbot callback

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def main():
     bot.set_first_stage(STAGE_ADMIN)
     bot.set_end_of_chatbot(
         lambda update, context: bot.edit_or_reply_message(
-            update, context, "You have exited the conversation. \n\nUse /start to begin a new one.")
+            update, context, "You have exited the conversation. \n\nUse /start to begin a new one.", reply_message=True)
     )
     bot.start(live_mode=LIVE_MODE)
 
