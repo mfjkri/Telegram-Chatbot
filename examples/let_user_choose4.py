65cb3d68958f141ce262efb67a2b1639ab1fe97e
mfjkri <muhammadfikri6844@gmail.com>
2022-07-24 15:59:14 +0800

    Added example with default answering of CallbackQueries disabled
    By default LetUserChoose answers the CallbackQuery for the choice selected. This can lead to rigidity or issues if a custom transition or behavior is wanted. As such you can disable this feature by setting answer_callback_query to be False when calling @Bot.let_user_choose.

---
+++ examples/let_user_choose4.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+import sys
+
+sys.path.append("src")
+
+import logging
+import os
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from constants import USERSTATE
+from bot import Bot
+from user import UserManager, User
+from utils import utils
+from utils.log import Log
+
+LOG_FILE = os.path.join("logs", f"example:let_user_choose4.log")
+
+CONFIG = utils.load_yaml_file(os.path.join("config.yaml"))
+assert CONFIG, "Failed to load config.yaml. Fatal error, please remedy."\
+    "\n\nLikely an invalid format."
+
+LIVE_MODE = CONFIG["RUNTIME"]["LIVE_MODE"]
+BOT_TOKEN = CONFIG["BOT_TOKENS"]["LIVE"] if LIVE_MODE else CONFIG["BOT_TOKENS"]["TEST"]
+
+
+def main():
+    logger = Log(
+        name=__name__,
+        stream_handle=sys.stdout,
+        file_handle=LOG_FILE,
+        log_level=logging.DEBUG
+    )
+
+    user_manager = UserManager()
+    user_manager.init(logger=logger)
+
+    bot = Bot()
+    bot.init(token=BOT_TOKEN,
+             logger=logger,
+             config=CONFIG)
+
+    STAGE_FAV_FRUIT = "choose_favorite_fruit"
+    STAGE_FAV_SHAPE = "choose_favorite_shape"
+    STAGE_END = "end"
+
+    # --------------------------- Stage: choose:fruit --------------------------- #
+    def handler_fruit_choice(choice: str, update: Update, context: CallbackContext) -> USERSTATE:
+        print(f"User has selected: {choice}")
+
+        query = update.callback_query
+        # We have to manually answer the CallbackQuery here
+        # since we set answer_callback_query=False (line #85)
+        # in this case, we are deleting the previous message
+        # (default behavior is a "💭 Loading..." transition message)
+        query.answer(keep_message="")
+
+        user: User = context.user_data.get("user")
+        user.update_user_data("favorite_fruit", choice)
+
+        return bot.proceed_next_stage(
+            current_stage_id=STAGE_FAV_FRUIT,
+            next_stage_id=STAGE_FAV_SHAPE,
+            update=update, context=context
+        )
+
+    bot.let_user_choose(
+        stage_id=STAGE_FAV_FRUIT,
+        choice_text="Which is your favorite fruit?",
+        choices=[
+            {
+                "text": "🍎",
+                "callback": lambda update, context: handler_fruit_choice("apple", update, context)
+            },
+            {
+                "text": "🍐",
+                "callback": lambda update, context: handler_fruit_choice("pear", update, context)
+            },
+            {
+                "text": "🍊",
+                "callback": lambda update, context: handler_fruit_choice("orange", update, context)
+            }
+        ],
+        answer_callback_query=False
+    )
+    bot.set_first_stage(stage_id=STAGE_FAV_FRUIT)
+    # ---------------------------------------------------------------------------- #
+
+    # ---------------------------- Stage: choose:shape --------------------------- #
+    def handler_shape_choice(choice: str, update: Update, context: CallbackContext) -> USERSTATE:
+        print(f"User has selected: {choice}")
+
+        query = update.callback_query
+        # We have to manually answer the CallbackQuery here
+        # since we set answer_callback_query=False (line #127)
+        # in this case, we are deleting the previous message
+        # (default behavior is a "💭 Loading..." transition message)
+        query.answer(keep_message="")
+
+        user: User = context.user_data.get("user")
+        user.update_user_data("favorite_shape", choice)
+
+        return bot.proceed_next_stage(
+            current_stage_id=STAGE_FAV_SHAPE,
+            next_stage_id=STAGE_END,
+            update=update, context=context
+        )
+
+    bot.let_user_choose(
+        stage_id=STAGE_FAV_SHAPE,
+        choice_text="Which is your favorite shape?",
+        choices=[
+            {
+                "text": "🟧",
+                "callback": lambda update, context: handler_shape_choice("square", update, context)
+            },
+            {
+                "text": "🔻",
+                "callback": lambda update, context: handler_shape_choice("triangle", update, context)
+            },
+            {
+                "text": "🟢",
+                "callback": lambda update, context: handler_shape_choice("circle", update, context)
+            }
+        ],
+        answer_callback_query=False
+    )
+    # ---------------------------------------------------------------------------- #
+
+    # -------------------------------- Stage: end -------------------------------- #
+    def display_favorites(update: Update, context: CallbackContext):
+        user: User = context.user_data.get("user")
+        fav_fruit_chosen: str = user.data.get("favorite_fruit")
+        fav_shape_chosen: str = user.data.get("favorite_shape")
+        bot.edit_or_reply_message(
+            update, context,
+            text=f"Your favorite fruit was: <b>{fav_fruit_chosen}</b>.\n"
+            f"Your favorite shape was: <b>{fav_shape_chosen}</b>.\n\n"
+            "You have exited the conversation.\nUse /start to begin a new one.",
+        )
+
+    bot.make_end_stage(
+        stage_id=STAGE_END,
+        final_callback=display_favorites
+    )
+    # ---------------------------------------------------------------------------- #
+
+    # Start Bot
+    logger.info(False, "")
+    logger.info(False, "Initializing...")
+    logger.info(False, "")
+
+    bot.start(live_mode=LIVE_MODE)
+
+
+if __name__ == "__main__":
+    main()
